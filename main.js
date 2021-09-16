b8bb9becdbad55ea46273119e862df4009c4d4a5
RodrigoMJ1 <rodrigo_1321124076@uptecamac.edu.mx>
2021-09-16 17:19:47 -0500

    Switch e if else

---
+++ main.js
@@ -0,0 +1,108 @@
+//var x = 300; // Variable para comparar
+//var y = 20;
+//var s = 30;
+//if(x < y){
+  //  console.log(x + " es menor a " + y);
+//}else{
+    //console.log(x +" es mayor a " + y);
+//}
+//var transporte = "autobus";
+
+//console.log("Paso 1: Salir a la entrada de mi casa");
+
+//if(transporte == "autobus"){
+   // console.log("Paso 2: Subirme al autobus");
+//}else{
+    //console.log("Paso 2: Subirme a la combi");
+//}
+
+//console.log("Paso 3: Bajar en paradero de Tecamac");
+//console.log("Paso 4: Caminar a la entrada de la uni");
+//console.log("Paso 5: Caminar al aula");
+//console.log("Paso 6: Entrar al aula y tomarlas clases");
+
+//var hermana="Jaz";
+//var mama="Vero";
+//var yo="Yo";
+
+//console.log("Mamá decide quien ira a las tortilas");
+//if(mama == "Vero"){
+  //  console.log("Máma ira a las tortillas");
+//}else{
+  //  console.log("Tiene ir a Jaz o yo");
+//}
+//if(hermana="Jaz"){
+  //  console.log("Jaz ira a las tortillas");
+//}else{
+  //  console.log("Tendre que ir yo");}
+var dinero= "25";
+  if (dinero=="5"){
+    console.log("Compro una paleta");                      
+  }
+  else if (dinero=="10") {
+    console.log("Compro dos paletas");
+  } else if (dinero=="15"){
+    console.log("Compro tres paletas");
+  }
+  else if (dinero=="20"){
+    console.log("Compro cuatro paletas");
+  }
+  else if (dinero=="25"){
+    console.log("Compro cinco paletas");
+  }
+
+  
+  var perro="Rocky";
+switch (perro) {
+    case "Gufy":
+        console.log(perro+" es mi perro");
+        break;
+    case "Chocolate":
+        console.log(perro+" es el perro de mi hermana");
+        break;
+    case "Oso":
+        console.log(perro+" es el perro de mi tia");
+        break;
+    case "Lucas":
+        console.log(perro+" es el perro de mi abuela");
+        break;
+    case "Rocky":
+        console.log(perro+" es el perro de mi primo");
+        break;
+    default:
+        console.log(perro+" no es un perro que conozca");
+        break;
+}
+
+var marca="Iphone";
+var color="Oro";
+switch (marca) {
+    case "Huawei":
+        if(color="Rojo"){
+            console.log("Tu telefono es marca "+ marca +" y es color "+ color);
+          }
+        break;
+    case "Samsung":
+        if(color="Azul"){
+            console.log("Tu telefono es marca "+ marca +" y es color "+ color);
+          }
+        break;
+    case "Iphone":
+         if(color="Oro"){
+            console.log("Tu telefono es marca "+ marca +" y es color "+ color);
+          }
+        break;
+    case "Motorola":
+        if(color="Plata"){
+            console.log("Tu telefono es marca "+ marca +" y es color "+ color);
+          }
+    case "Nokia": 
+        if(color="Rosa"){
+            console.log("Tu telefono es marca "+ marca +" y es color "+ color);
+          }
+    default:
+        console.log(marca +" es una marca no reconocida y el color"+ color + "no");
+        break;
+}
+    
+
